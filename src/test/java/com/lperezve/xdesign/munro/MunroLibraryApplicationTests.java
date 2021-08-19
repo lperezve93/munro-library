ac164e952498935ecdfedf54777f89b9fa615338
lorenapv <lorenapv93@gmail.com>
2021-08-19 22:38:28 +0100

    Initial Commit - environment set up

---
+++ src/test/java/com/lperezve/xdesign/munro/MunroLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lperezve.xdesign.munro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunroLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
